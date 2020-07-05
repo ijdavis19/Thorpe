4330be0aaebea557b866cdafd51be01babf6a20b
Ian Davis <ijdavis@g.clemson.edu>
2020-07-05 12:23:14 -0500

    Added a main function for readability(?)

--- singleGameSummary/singleGameSummary.py
+++ singleGameSummary/singleGameSummary.py
@@ -7,39 +7,45 @@ from gameIDFinder import gameIDFinder
 import io
 import os
 
-# dfGame = cfb.get_game_info(year=2015,week=5,team='Clemson')
-gameID, yearInput, week, team1 = gameIDFinder()
-print("\nGame ID: {}".format(gameID))
-print("\nYear: {}".format(yearInput))
-
-thisGame = game.Game(year=yearInput, gameID=gameID, week=week, team=team1)
-
-print("Editing PDF...")
-
-outFileString = thisGame.singleGameFileName()
-
-with open('output/singleGameSummary.tex', 'r') as file:
-    inFile = file.readlines()
 
 
+def main():
+    # dfGame = cfb.get_game_info(year=2015,week=5,team='Clemson')
+    gameID, yearInput, week, team1 = gameIDFinder()
+    print("\nGame ID: {}".format(gameID))
+    print("\nYear: {}".format(yearInput))
 
-if os.path.isfile("{}.tex".format(outFileString)):
-    os.remove("{}.tex".format(outFileString))
+    thisGame = game.Game(year=yearInput, gameID=gameID, week=week, team=team1)
 
-outFile = open("{}.tex".format(outFileString), 'w')
+    print("Editing PDF...")
 
-thisGame.writeToLatex(inFile=inFile, outFile=outFile, varlist=thisGame.pairs)
+    outFileString = thisGame.singleGameFileName()
+
+    with open('output/singleGameSummary.tex', 'r') as file:
+        inFile = file.readlines()
 
 
-if os.path.isfile("{}.tex".format(outFileString)):
-    print("File built successfully")
 
-os.system("pdflatex {}.tex".format(outFileString))
-os.system("pdflatex {}.tex".format(outFileString))
-os.system("rm {}.out".format(outFileString))
-os.system("rm {}.log".format(outFileString))
-os.system("rm {}.aux".format(outFileString))
-os.system("rm {}.toc".format(outFileString))
-os.system("rm {}.tex".format(outFileString))
-os.system("mv {}.pdf output/".format(outFileString))
-os.system("xdg-open output/{}.pdf".format(outFileString))
+    if os.path.isfile("{}.tex".format(outFileString)):
+        os.remove("{}.tex".format(outFileString))
+
+    outFile = open("{}.tex".format(outFileString), 'w')
+
+    thisGame.writeToLatex(inFile=inFile, outFile=outFile, varlist=thisGame.pairs)
+
+
+    if os.path.isfile("{}.tex".format(outFileString)):
+        print("File built successfully")
+
+    os.system("pdflatex {}.tex".format(outFileString))
+    os.system("pdflatex {}.tex".format(outFileString))
+    os.system("rm {}.out".format(outFileString))
+    os.system("rm {}.log".format(outFileString))
+    os.system("rm {}.aux".format(outFileString))
+    os.system("rm {}.toc".format(outFileString))
+    os.system("rm {}.tex".format(outFileString))
+    os.system("mv {}.pdf output/".format(outFileString))
+    os.system("xdg-open output/{}.pdf".format(outFileString))
+
+if __name__ == '__main__':
+    main()
--- test/localGameCreator.py
+++ test/localGameCreator.py
@@ -5,5 +5,9 @@ import CFBScrapy as CFB
 import game
 
 print("start")
+print("import pandas as pd")
+print("import CFBScrapy as CFB")
+print("import game")
 thisGame = game.Game(year=2019, gameID=401112456, week=4, team='Clemson')
+print("thisGame = game.Game(year=2019, gameID=401112456, week=4, team='Clemson')")
 print("succ")
